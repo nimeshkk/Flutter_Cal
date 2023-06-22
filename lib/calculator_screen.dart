7746ce6da0f9894942262e105b0565da50acad1e
nimeshkk <nimeshkliyanage@gmail.com>
2023-06-22 22:53:10 +0530

    update

--- lib/calculator_screen.dart
+++ lib/calculator_screen.dart
@@ -8,11 +8,10 @@ class Calculator extends StatefulWidget {
 }
 
 class _CalculatorState extends State<Calculator> {
-  @override
-  String userInput="";
-  String result="0";
+  String userInput = "";
+  String result = "0";
 
-  List<String> buttonList=[
+  List<String> buttonList = [
     'AC',
     '(',
     ')',
@@ -33,76 +32,121 @@ class _CalculatorState extends State<Calculator> {
     '0',
     '.',
     '=',
-
   ];
-     
 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 40, 38, 33),
       body: Column(
-       
         children: [
           SizedBox(
-            height:MediaQuery.of(context).size.height/3,
-            child:Column( mainAxisAlignment: MainAxisAlignment.end, children:[
-
-              Container(
-                padding: EdgeInsets.all(15),
-                alignment: Alignment.centerRight,
-                child: Text(
-                  userInput,
-                style: TextStyle(fontSize: 32,color: Colors.white),),
-              ),
-
-              Container(
-                padding: EdgeInsets.all(20),
-                alignment: Alignment.centerRight,
-                child: Text(
-                  result,
-                style: TextStyle(fontSize: 48,color: Colors.white,fontWeight: FontWeight.bold),),
-              ),
-
-
-            ]),
+            height: MediaQuery.of(context).size.height / 3,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  padding: EdgeInsets.all(15),
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    userInput,
+                    style: TextStyle(fontSize: 32, color: Colors.white),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(20),
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    result,
+                    style: TextStyle(
+                        fontSize: 48,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
             ),
-
-            Divider(color: Colors.white),
-            Expanded(
-              child: Container(
+          ),
+          Divider(color: Colors.white),
+          Expanded(
+            child: Container(
               padding: EdgeInsets.all(10),
-              child:GridView.builder(
+              child: GridView.builder(
                 itemCount: buttonList.length,
                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                   crossAxisCount: 4,
                   crossAxisSpacing: 10,
                   mainAxisSpacing: 12,
-                  ),
-                 itemBuilder:(BuildContext context, int index){
-
+                ),
+                itemBuilder: (BuildContext context, int index) {
                   return CustomButton(buttonList[index]);
-                 }
-                   
-                 
-                 ),
-
-            )),
-          
-        ],),
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
-  Widget CustomButton(String Text){
+
+  Widget CustomButton(String text) {
     return InkWell(
-      splashColor: Colors.blueGrey,
-      onTap: (){},
-      child:Ink(
+      splashColor: const Color.fromARGB(255, 255, 255, 255),
+      onTap: () {},
+      child: Ink(
         decoration: BoxDecoration(
-          color: Colors.blue,
+          color: getBgColor(text),
           borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+              color: Color.fromARGB(255, 0, 0, 0).withOpacity(0.5),
+              spreadRadius: 1,
+              blurRadius: 10,
+              offset: Offset(-3, -3),
+            ),
+          ],
+        ),
+        child: Center(
+          child: Text(
+            text,
+            style: TextStyle(
+              color: getColor(text),
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
         ),
       ),
-
     );
   }
-  
+
+  getColor(String text) {
+    if (text == '/' ||
+        text == '*' ||
+        text == '+' ||
+        text == '-' ||
+        text == 'c' ||
+        text == '(' ||
+        text == ')') {
+      return const Color.fromARGB(255, 244, 244, 244);
+
+    }  
+      return const Color.fromARGB(255, 118, 118, 118);
+     }
+
+     getBgColor(String text) {
+    if (text == '/' ||
+        text == '*' ||
+        text == '+' ||
+        text == '-' ||
+        text == 'c' ||
+        text == '(' ||
+        text == ')') {
+      return Colors.orange;
+
+    }  
+      return Color.fromARGB(255, 34, 23, 9);
+      
+    }
 }
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'calculator_screen.dart';
+import 'package:device_preview/device_preview.dart';
 
-void main() {
-  runApp( MyApp());
-}
+void main() => runApp(
+  DevicePreview(
+   
+    builder: (context) => MyApp(), // Wrap your app
+  ),
+);
 
 class MyApp extends StatelessWidget {
  
@@ -12,7 +16,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       debugShowCheckedModeBanner: false,
       home:Calculator(),
     );
